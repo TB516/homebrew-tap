466ccead82fc8267a31661f91e289456fe9c51cc
Thomas <77514919+TB516@users.noreply.github.com>
2026-04-24 00:19:13 -0400

    Updated cask

--- Casks/pnpm-standalone.rb
+++ Casks/pnpm-standalone.rb
@@ -1,9 +1,10 @@
 cask "pnpm-standalone" do
   arch arm: "arm64", intel: "x64"
+  os linux: "linux"
 
   version "10.33.2"
   sha256 arm64_linux: "0828e5ee23be89d22bd53cc36e93c181ce9d5c47d75f9fe9bf4bdc7a65c66322",
-         x64_linux:  "39d7b6600239712bc9581ea219b17ffef46ba60998779cb717be2e068be029ef"
+         x86_64_linux:  "39d7b6600239712bc9581ea219b17ffef46ba60998779cb717be2e068be029ef"
 
   url "https://github.com/pnpm/pnpm/releases/download/v#{version}/pnpm-linux-#{arch}"
   name "pnpm (standalone)"

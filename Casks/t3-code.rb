05301e4b5275e068cdee26cace0fdce2113060c9
Thomas <77514919+TB516@users.noreply.github.com>
2026-04-24 23:32:17 -0400

    Add t3code (#3)
    * Add t3code cask
    
    * updated cask name
    
    * Updated caskfile to store desktop, wrapper, and icons in persistent location
    
    * Fixed t3code install and added postflight cleanup
    
    * Updated to use unpacked appimage internals to launch instead of the raw appimage
    
    * Updated icon path to allow it to display
    
    * Fixed style errors

---
+++ Casks/t3-code.rb
@@ -0,0 +1,50 @@
+cask "t3-code" do
+  arch intel: "x86_64"
+  os linux: "linux"
+
+  version "0.0.21"
+  sha256 "79009fb24a65f8924ec9a618ee88180a2d190ae58d45c129b1e58c9e24bf2c24"
+
+  url "https://github.com/pingdotgg/t3code/releases/download/v#{version}/T3-Code-#{version}-#{arch}.AppImage"
+  name "T3 Code"
+  desc "Minimal web GUI for coding agents"
+  homepage "https://github.com/pingdotgg/t3code"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  binary "squashfs-root/t3code", target: "t3code"
+  artifact "squashfs-root/t3code.desktop",
+           target: "#{ENV["XDG_DATA_HOME"] || "#{Dir.home}/.local/share"}/applications/t3code.desktop"
+  artifact "squashfs-root/usr/share/icons/hicolor/1024x1024/apps/t3code.png",
+           target: "#{ENV["XDG_DATA_HOME"] || "#{Dir.home}/.local/share"}/icons/hicolor/512x512/apps/t3code.png"
+
+  preflight do
+    appimage_name = "T3-Code-#{version}-#{arch}.AppImage"
+    appimage = "#{staged_path}/#{appimage_name}"
+
+    system("chmod", "+x", appimage)
+
+    Dir.chdir(staged_path) do
+      system("./#{appimage_name}", "--appimage-extract")
+    end
+
+    xdg_data_home = ENV["XDG_DATA_HOME"] || "#{Dir.home}/.local/share"
+    FileUtils.mkdir_p("#{xdg_data_home}/applications")
+    FileUtils.mkdir_p("#{xdg_data_home}/icons/hicolor/1024x1024/apps")
+
+    desktop_file = "#{staged_path}/squashfs-root/t3code.desktop"
+    desktop_contents = File.read(desktop_file)
+
+    desktop_contents.gsub!(/^Exec=.*$/,
+                           "Exec=env T3CODE_HOME=#{xdg_data_home}/t3code T3CODE_DISABLE_AUTO_UPDATE=1 #{HOMEBREW_PREFIX}/bin/t3code %U")
+    desktop_contents.gsub!(/^Name=.*$/, "Name=T3 Code")
+    desktop_contents.gsub!(/^X-AppImage-Version=.*\n/, "")
+
+    File.write(desktop_file, desktop_contents)
+  end
+
+  zap delete: "#{ENV["XDG_DATA_HOME"] || "#{Dir.home}/.local/share"}/t3code"
+end

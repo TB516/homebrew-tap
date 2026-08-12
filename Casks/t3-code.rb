b612bee79de0ecd88ef57a6223f7ce7f0fb5ba6d
Thomas <77514919+TB516@users.noreply.github.com>
2026-08-12 17:42:07 -0400

    Update T3 Code zap paths

--- Casks/t3-code.rb
+++ Casks/t3-code.rb
@@ -51,6 +51,7 @@ cask "t3-code" do
   end
 
   zap trash: [
+    "#{Dir.home}/.t3",
     "#{ENV["XDG_DATA_HOME"] || "#{Dir.home}/.local/share"}/t3code",
     "#{ENV["XDG_CONFIG_HOME"] || "#{Dir.home}/.config"}/t3code",
   ]
